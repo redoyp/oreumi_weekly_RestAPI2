90b7a4596848ca79d39f14eda9459eabc682b193
redoyp <86337937+redoyp@users.noreply.github.com>
2025-05-20 11:10:58 +0900

    Update WeeklyRest2ApplicationTests.java

--- src/test/java/com/example/weeklyrest2/WeeklyRest2ApplicationTests.java
+++ src/test/java/com/example/weeklyrest2/WeeklyRest2ApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.weeklyrest2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class WeeklyRest2ApplicationTests {
 
